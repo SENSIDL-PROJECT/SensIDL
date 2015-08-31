be982cdf9da37e2f82cfcf05ce63aabcbc32c1c0
scheerer <scheerer@fzi.de>
2015-08-31 15:57:18 +0200

    adapt the textual editor

--- code/specification/de.fzi.sensidl.language/src/de/fzi/sensidl/language/converters/SensIDLValueConverterService.java
+++ code/specification/de.fzi.sensidl.language/src/de/fzi/sensidl/language/converters/SensIDLValueConverterService.java
@@ -4,6 +4,8 @@ import org.eclipse.xtext.common.services.DefaultTerminalConverters;
 import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
 
+import javax.measure.unit.Unit;
+
 import com.google.inject.Inject;
 
 /**
@@ -12,13 +14,23 @@ import com.google.inject.Inject;
  * Bietet zusätzlich einen {@link IValueConverter} für {@link Double Doubles} an.
  * @see DOUBLEValueConverter
  * @author Dominik Werle
+ * @author Max Scheerer
+ * @author Emre Taspolatoglu
  */
 public class SensIDLValueConverterService extends DefaultTerminalConverters {
-	@Inject
-	private DOUBLEValueConverter doubleValueConverter;
-	
-	@ValueConverter(rule = "DOUBLE")
-	public IValueConverter<Double> DOUBLE() {
-		return doubleValueConverter;
-	}
+    @Inject
+    private DOUBLEValueConverter doubleValueConverter;
+    @Inject
+    private JScienceUnitValueConverter jScienceUnitValueConverter;
+    
+    @ValueConverter(rule = "DOUBLE")
+    public IValueConverter<Double> DOUBLE() {
+           return doubleValueConverter;
+    }
+    
+    @ValueConverter(rule = "UNIT")
+    public IValueConverter<Unit> UNIT() {
+           return jScienceUnitValueConverter;
+    }
 }
+
