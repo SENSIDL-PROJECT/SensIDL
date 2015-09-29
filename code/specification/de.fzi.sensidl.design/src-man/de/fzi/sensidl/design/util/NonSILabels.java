f757c7393db7dfc4fb196eb73e960c6e5b25e54e
SvenEckhardt <sven_eckhardt@web.de>
2015-09-29 13:18:35 +0200

    Done #2: NonSi-Labels Class is active

--- code/specification/de.fzi.sensidl.design/src-man/de/fzi/sensidl/design/util/NonSILabels.java
+++ code/specification/de.fzi.sensidl.design/src-man/de/fzi/sensidl/design/util/NonSILabels.java
@@ -1,5 +1,7 @@
 package de.fzi.sensidl.design.util;
 
+import javax.measure.unit.NonSI;
+import javax.measure.unit.SI;
 import javax.measure.unit.UnitFormat;
 
 /**
@@ -19,6 +21,12 @@ public class NonSILabels {
 		UnitFormat format = UnitFormat.getInstance();
 //		Possible usage
 //		format.label(arg0, arg1);
+		format.label(NonSI.BYTE, "B");
+		format.label(SI.KILO(NonSI.BYTE), "kB");
+		format.label(SI.MEGA(NonSI.BYTE), "MB");
+		format.label(SI.GIGA(NonSI.BYTE), "GB");
+		format.label(SI.TERA(NonSI.BYTE), "TB");
+		format.label(SI.PETA(NonSI.BYTE), "PB");
 		
 	}
 }
