6a3b03c14d863a369a2dd238af3673d0289ff1da
SvenEckhardt <sven_eckhardt@web.de>
2015-11-24 11:27:25 +0100

    Add Test for automatic build process

---
+++ code/specification/de.fzi.sensidl.language.tests/src/de/fzi/sensidl/language/tests/DummyTest.java
@@ -0,0 +1,14 @@
+package de.fzi.sensidl.language.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+	
+	@Test
+	public void test_true(){
+		assertTrue(true);
+	}
+
+}
