7a2dc5971ac76fd60b4c711cf8ac609d1709b65a
FabianScheytt <uudwo@student.kit.edu>
2016-06-15 12:44:04 +0200

    Corrected a type conversion bug within the Arduino Sensor

--- code/demonstration/Arduino/Umgebungskachel/UmgebungskachelCommunication.ino
+++ code/demonstration/Arduino/Umgebungskachel/UmgebungskachelCommunication.ino
@@ -9,9 +9,9 @@ String datastructureToJson() {
   
         DynamicJsonBuffer jsonBuffer;
         JsonObject& root = jsonBuffer.createObject();
-        JsonObject& ledToggle = root.createNestedObject("LedToggle");
-        JsonObject& alertTemp = root.createNestedObject("AlertThresholdTemperature");
-        JsonObject& alertBright = root.createNestedObject("AlertThresholdBrightness");
+        JsonObject& ledToggle = root.createNestedObject("ledToggle");
+        JsonObject& alertTemp = root.createNestedObject("alertThresholdTemperature");
+        JsonObject& alertBright = root.createNestedObject("alertThresholdBrightness");
         
         ledToggle["led"] = get_SensorState_led(&sensorState);
         alertTemp["threshold_temperature"] = get_SensorState_thresholdtemperature(&sensorState);
@@ -24,15 +24,6 @@ String datastructureToJson() {
         char json[len+1];
         root.prettyPrintTo(json, sizeof(json));
         return json;
-        /*String json = "{";
-	json += "\"led\":\"" + (String)get_SensorState_led(&sensorState) + "\",";
-	json += "\"temperature_f\":"+ dtostrf(get_SensorState_temperaturef(&sensorState),0,4) + ",";
-        json += "\"temperature_c\":"+ dtostrf(get_SensorState_temperaturec(&sensorState),0,4) + ",";
-	json += "\"brightness\":" + dtostrf(get_SensorState_brightness(&sensorState),0,4) + ",";
-	json += "\"threshold_brightness\":"+ dtostrf(get_SensorState_thresholdbrightness(&sensorState),0,4) + ",";
-	json += "\"threshold_temperature\":"+ dtostrf(get_SensorState_thresholdtemperature(&sensorState),0,4);
-	json += "}";
-	return json;*/
 }
 
 void parseDatastructureFromJson(String json) {
@@ -45,21 +36,17 @@ void parseDatastructureFromJson(String json) {
           Serial.println("parseObject() failed");
           return;
         }
+        Serial.println(json);
         if(root.containsKey("led")) {
 		if(((String)root["led"].asString()).equals("ON")) set_SensorState_led(&sensorState,"ON");
 		if(((String)root["led"].asString()).equals("OFF")) set_SensorState_led(&sensorState,"OFF");
+                Serial.println(root["led"].asString());
 	} 
-	if (root.containsKey("temperaturec")) {			
-		set_SensorState_temperaturec(&sensorState,stringToDouble(root["temperaturec"].asString()));
-	} 
-	if (root.containsKey("brightness")) {
-		set_SensorState_brightness(&sensorState,stringToDouble(root["brightness"].asString()));
-	}
 	if (root.containsKey("threshold_brightness")) {
-		set_SensorState_thresholdbrightness(&sensorState,stringToDouble(root["threshold_brightness"].asString()));
+		set_SensorState_thresholdbrightness(&sensorState,(double)root["threshold_brightness"]);
 	}
 	if (root.containsKey("threshold_temperature")) {
-		set_SensorState_thresholdtemperature(&sensorState,stringToDouble(root["threshold_temperature"].asString()));
+		set_SensorState_thresholdtemperature(&sensorState,(double)root["threshold_temperature"]);
 	}
 }
 
